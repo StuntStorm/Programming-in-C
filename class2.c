712ba75be930ab6db2c4eeefe475e75542761818
Rizwan Nizarudin <rizwan.nizarudin@gmail.com>
2022-03-30 10:40:47 +0530

    Class #2 over

---
+++ class2.c
@@ -0,0 +1,11 @@
+/*Here is a testing of how to write multi-line commenting
+in C Programming*/
+#include<stdio.h>
+int main()
+{
+  int a, b, c; //Decleration Statement
+  a = 10;
+  b = 20;
+  c = a + b;
+  printf("%d",c);
+}
---
+++ class2HW.c
@@ -0,0 +1,12 @@
+// HW : Can _ Only be be used as a Variable?
+
+// Answer :
+
+#include<stdio.h>
+int main()
+{
+  int _,a,c; //Decleration Statement
+  _ = 8;
+  printf("%d",_);
+}
+// Reason :  Because _ is considered as a Variable
