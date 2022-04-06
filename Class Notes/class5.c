65d4bd70615dd853d0fdec0b22f4c3372faeb688
StuntStorm <rizwan.nizarudin@gmail.com>
2022-04-06 04:01:06 +0000

    updates

---
+++ Class Notes/class5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    //Modules Only Works for int and char!
+    char a ='a', b = 'A';
+    printf("Reminder : %d\n",a%b);
+
+    // A program to print the number of characters in a string
+    int s;
+    s = printf("Amrita\n");
+    printf("No. of Characters : %d\n",s);
+
+}
