6229cd16b6555e7af68fb05086e10fee33d43d16
StuntStorm <rizwan.nizarudin@gmail.com>
2022-04-12 14:10:13 +0000

    updates

--- LAB-3/13.modfrog.c
+++ LAB-3/13.modfrog.c
@@ -3,9 +3,19 @@ Starting at point 0 on the x-axis, it jumps b units to the left.
 Your task is to calculate the position of the frog after k jumps.*/
 
 #include <stdio.h>
+
 int main()
 {
-    int a,k;
-    scanf("%d%d",&a,&k);
-    printf("%d",-a*k);
+    int t,i;
+    unsigned long long int a,b,k,d;
+    scanf("%d",&t);
+    for (i=0;i<t;i++)
+    {
+        scanf("%lli%lli%lli",&a,&b,&k);
+        d=a-b;
+        if (k%2==0)
+            printf("%lli\n",(k/2)*d);
+        else
+            printf("%lli\n",(k/2)*d+a);
+    }
 }
