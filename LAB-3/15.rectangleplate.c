9a230e7c95c029b20c6b6210bc4ec29f65798de5
StuntStorm <rizwan.nizarudin@gmail.com>
2022-04-12 12:06:20 +0000

    Updates

--- LAB-3/15.rectangleplate.c
+++ LAB-3/15.rectangleplate.c
@@ -2,9 +2,16 @@
 the bordering cells so that it forms a golden ring. The plate is a rectangle
 that we split into w x h cells. Your task is to compute the number of cells gilded.*/
 
+#include <stdio.h>
 int main()
 {
-    int w,h;
-    scanf("%d%d",&w,&h);
-    printf("%d",2*w + 2*h - 4);
+    int w,h,k,result,i;
+    scanf("%d%d%d",&w,&h,&k);
+    result=2*w + 2*h - 4;
+    for (i=1; i<k; ++i){
+        w=w-4;
+        h=h-4;
+        result = result + (2*w+2*h-4);
+    };
+    printf("%d",result);
 }
