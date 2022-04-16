ad84445d7afb35e2ac497b4061c185dcd7ccbb41
Rizwan Nizarudin <56226566+StuntStorm@users.noreply.github.com>
2022-04-16 23:21:28 +0530

    Delete LAB-2 directory

--- LAB-2/6.3printf.c
+++
@@ -1,9 +0,0 @@
-//Write a Program to print * cone using 3 printf
-
-#include<stdio.h>
-int main()
-{
-    printf("*\n");
-    printf("**\n");
-    printf("***\n");
-}
--- LAB-2/7.1printf.c
+++
@@ -1,7 +0,0 @@
-//Write a Program to print * cone using 1 printf
-
-#include<stdio.h>
-int main()
-{
-    printf("*\n**\n***\n");
-}
