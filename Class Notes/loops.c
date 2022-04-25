e252d4891294abfcdd2f42cefa33491695123f01
StuntStorm <rizwan.nizarudin@gmail.com>
2022-04-25 06:19:55 +0000

    Updates

---
+++ Class Notes/loops.c
@@ -0,0 +1,88 @@
+//Demonstration of loops in C
+
+/*
+Different types of loops in C
+1. For loop
+2. While loop
+3. do - while loop
+*/
+
+#include<stdio.h>
+int main()
+{
+
+    /*for (initialization;condition checking;loop variable update)
+    {
+
+    }*/
+
+    /*
+    for(int i = 1;i<=10;i++)
+    {
+        printf("StuntStorm\n");
+    }
+    printf("Bye\n");
+    */
+
+
+
+    /*
+    int i = 1;
+    for(;i<=10;i++)
+    {
+        printf("StuntStorm\n");
+    }
+    */
+
+    /*
+    int i = 1;
+    for(;i<=10;i++)
+    {
+        printf("StuntStorm\n");
+    }
+    printf("Bye\n");
+    */
+
+
+    /*
+    int i = 1;
+    for(;;)
+    {
+        if(i<=10)
+        {
+            printf("StuntStorm\n");
+            i++;
+        }
+    }
+    */
+
+
+    /*
+    int i = 1;
+    for(;;)
+    {
+        if(i<=10)
+        {
+            printf("StuntStorm\n");
+            i++;
+        }
+        else
+        {
+            break;
+        }
+    }
+    printf("Bye\n");
+    */
+
+
+    // PRINT ALL NUMBER USING LOOP
+    for(int i = 0;i<=10;i++)
+    {
+        printf("%d ",i);
+    }
+    
+
+
+
+
+}
